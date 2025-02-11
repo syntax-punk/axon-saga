1358a76defe2e850fea591d3a35279980bf86f97
syntax-punk <david.jaeren@gmail.com>
2025-02-11 08:27:08 +0100

    Added eureka client settings to the Products Service to register it in the Eureka discovery service

--- ProductService/src/main/java/no/syntaxpunk/cqrsestore/ProductService/rest/ProductController.java
+++ ProductService/src/main/java/no/syntaxpunk/cqrsestore/ProductService/rest/ProductController.java
@@ -1,5 +1,6 @@
 package no.syntaxpunk.cqrsestore.ProductService.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @GetMapping
+    public String getProducts() {
+        return "getting products";
+    }
 
     @PostMapping
     public String createProduct() {
