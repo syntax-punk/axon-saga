5cd767fdd63872c84358fc7ba4d64cfdb54e752c
syntax-punk <david.jaeren@gmail.com>
2025-02-12 20:46:38 +0100

    Testin API gateway

--- ProductService/src/main/java/no/syntaxpunk/cqrsestore/ProductService/rest/ProductController.java
+++ ProductService/src/main/java/no/syntaxpunk/cqrsestore/ProductService/rest/ProductController.java
@@ -1,5 +1,7 @@
 package no.syntaxpunk.cqrsestore.ProductService.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    Environment env;
+
     @GetMapping
     public String getProducts() {
-        return "getting products";
+        return "getting products: " + env.getProperty("local.server.port");
     }
 
     @PostMapping
