9b3f929bbda1be4871198e50a7350dc96926fc1f
syntax-punk <david.jaeren@gmail.com>
2025-02-15 21:59:28 +0100

    Added a commandgateway and firing the command

--- ProductService/src/main/java/no/syntaxpunk/cqrsestore/ProductService/rest/ProductController.java
+++ ProductService/src/main/java/no/syntaxpunk/cqrsestore/ProductService/rest/ProductController.java
@@ -1,15 +1,24 @@
 package no.syntaxpunk.cqrsestore.ProductService.rest;
 
+import no.syntaxpunk.cqrsestore.ProductService.command.CreateProductCommand;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-    @Autowired
-    Environment env;
+    private final Environment env;
+    private final CommandGateway commandGateway;
+
+    public ProductController(Environment env, CommandGateway commandGateway) {
+        this.env = env;
+        this.commandGateway = commandGateway;
+    }
 
     @GetMapping
     public String getProducts() {
@@ -18,6 +27,21 @@ public class ProductController {
 
     @PostMapping
     public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
-        return "product created: " + createProductRestModel.getTitle();
+        var createProductCommand = CreateProductCommand.builder()
+            .title(createProductRestModel.getTitle())
+            .price(createProductRestModel.getPrice())
+            .quantity(createProductRestModel.getQuantity())
+            .productId(UUID.randomUUID().toString())
+            .build();
+
+        String returnValue;
+        
+        try {
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception ex) {
+            returnValue = ex.getLocalizedMessage();
+        }
+
+        return returnValue;
     }
 }
