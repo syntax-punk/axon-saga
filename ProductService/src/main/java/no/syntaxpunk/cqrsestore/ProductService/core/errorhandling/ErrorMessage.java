6f514ac56e518c5379e3946c6762ee9ef14212cf
syntax-punk <david.jaeren@gmail.com>
2025-03-08 13:13:35 +0100

    Added an error message to return it to the client app

---
+++ ProductService/src/main/java/no/syntaxpunk/cqrsestore/ProductService/core/errorhandling/ErrorMessage.java
@@ -0,0 +1,13 @@
+package no.syntaxpunk.cqrsestore.ProductService.core.errorhandling;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessage {
+    private final Date timestamp;
+    private final String message;
+}
